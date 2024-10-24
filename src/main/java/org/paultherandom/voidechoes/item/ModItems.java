8a6b1028f09d09a16591ae187d55178ed04b66d6
Paul; the Random <paul.alvarez.ramos@icloud.com>
2024-10-23 20:32:43 -0600

    add: basic item generation methods and class.

---
+++ src/main/java/org/paultherandom/voidechoes/item/ModItems.java
@@ -0,0 +1,29 @@
+package org.paultherandom.voidechoes.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.paultherandom.voidechoes.EchoesOfTheVoid;
+
+public class ModItems {
+    public static final Item SHADOW_FABRIC = itemRegister("shadow_fabric", new Item(new FabricItemSettings()));
+
+    private static void addItemToIngredientTab(FabricItemGroupEntries entries) {
+        entries.add(SHADOW_FABRIC);
+    }
+
+    private static Item itemRegister(String id, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(EchoesOfTheVoid.MOD_ID, id), item);
+    }
+
+    public static void modItemRegister() {
+        EchoesOfTheVoid.LOGGER.info("Registering Mod Items for " + EchoesOfTheVoid.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToIngredientTab);
+    }
+}
