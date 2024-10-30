8571ecd71c4d94a6d12a93196f6b9f71c3c1bc6a
Paul; the Random <paul.alvarez.ramos@icloud.com>
2024-10-30 15:10:11 -0600

    add: class for future custom properties of shadow fabric.

---
+++ src/main/java/org/paultherandom/voidechoes/item/custom/ShadowFabricItem.java
@@ -0,0 +1,11 @@
+package org.paultherandom.voidechoes.item.custom;
+
+import net.minecraft.item.Item;
+
+public class ShadowFabricItem extends Item {
+    public ShadowFabricItem(Settings settings) {
+        super(settings);
+    }
+
+    
+}
