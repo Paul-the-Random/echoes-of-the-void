ecd00c84a8b2b7d84d7c1e5b026da5a37a6cc28a
Paul; the Random <paul.alvarez.ramos@icloud.com>
2024-10-31 10:28:28 -0600

    add: ModRecipeProvider logic for the first recipes.

--- src/main/java/org/paultherandom/voidechoes/datagen/ModRecipeProvider.java
+++ src/main/java/org/paultherandom/voidechoes/datagen/ModRecipeProvider.java
@@ -3,6 +3,11 @@ package org.paultherandom.voidechoes.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import org.paultherandom.voidechoes.item.ModItems;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
     public ModRecipeProvider(FabricDataOutput output) {
@@ -11,6 +16,25 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter recipeExporter) {
-
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.SHADOW_SEAL, 1)
+                .pattern("BEB")
+                .pattern("BAB")
+                .pattern("BBB")
+                .input('A', Items.AMETHYST_SHARD)
+                .input('E', Items.ENDER_EYE)
+                .input('B', Items.POLISHED_DEEPSLATE)
+                .criterion(hasItem(ModItems.SHADOW_FABRIC), conditionsFromItem(ModItems.SHADOW_FABRIC))
+                .offerTo(recipeExporter, new Identifier(getRecipeName(ModItems.SHADOW_SEAL)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.SHADOW_EXTRACTOR, 1)
+                .pattern("#O#")
+                .pattern("NSN")
+                .pattern("#D#")
+                .input('D', Items.DIAMOND)
+                .input('O', ModItems.OMINOUS_MOLD)
+                .input('S', Items.SCULK_CATALYST)
+                .input('N', Items.NETHERITE_SCRAP)
+                .criterion(hasItem(ModItems.SHADOW_FABRIC), conditionsFromItem(ModItems.SHADOW_FABRIC))
+                .criterion(hasItem(ModItems.OMINOUS_MOLD), conditionsFromItem(ModItems.OMINOUS_MOLD))
+                .offerTo(recipeExporter, new Identifier(getRecipeName(ModItems.SHADOW_EXTRACTOR)));
     }
 }
